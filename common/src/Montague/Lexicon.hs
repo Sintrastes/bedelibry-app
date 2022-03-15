5c66a517cff0c8392f79711c1d6245d9d70c68e0
Nathan Bedell <nbedell@tulane.edu>
2022-03-15 14:16:32 -0500

    More schema implementation work.

--- common/src/Montague/Lexicon.hs
+++ common/src/Montague/Lexicon.hs
@@ -3,8 +3,8 @@ module Montague.Lexicon where
 
 import Montague.Types
 import Montague.Semantics
--- import Text.Parsec
--- import Distribution.Compat.CharParsing (sepBy)
+import Text.Parsec hiding (token, ParseError)
+import Text.ParserCombinators.Parsec.Char
 import GHC.Real (odd)
 import Data.Proxy
 import GHC.TypeLits
@@ -12,28 +12,15 @@ import Data.PartialOrd hiding ((==))
 import Data.Void
 import Data.Function
 
--- | Raw representation of a parsed montague lexicon file.
-data LexiconFile a t = LexiconFile {
-    entries :: [LexiconEntry a t]
-}
-
-data SomeLexiconFile = forall a t. SomeLexiconFile (LexiconFile a t)
-
--- Representation of an entry in a montague lexicon.
-data LexiconEntry a t = LexiconEntry {
-    words :: [String],
-    types :: [Term a t]
-}
+------------- Public API --------------
 
 -- | Load a montague lexicon from a file.
-getLexicon :: FilePath -> IO (Lexicon a t)
-getLexicon = undefined
+loadLexicon :: FilePath -> IO SomeLexicon
+loadLexicon = undefined
 
-parseLexicon :: (String -> a) -> String -> Maybe (LexiconFile a t)
-parseLexicon parseAtom x = undefined
-
-parseLexiconEntry :: (String -> a) -> String -> Maybe (LexiconEntry a t)
-parseLexiconEntry parseAtom x = undefined
+-- | Load a montague lexicon from it's contents.
+parseLexicon :: String -> Maybe SomeLexicon
+parseLexicon x = undefined
 
 ------------- Type Parsing ------------
 
@@ -58,19 +45,31 @@ parseTypeLexicon :: [String] -> SomeTypeLexicon
 parseTypeLexicon [x] = someSymbolVal x & \case
   SomeSymbol sym -> SomeTypeLexicon {
     parseType = \i -> if i == x
-        then Just (ShowableType sym) 
+        then Just $ SEOne sym
         else Nothing
   }
-parseTypeLexicon _ = undefined
+-- I think the real way to decode this would be via first parsing
+-- the list into a vector.
+parseTypeLexicon (x:xs) = someSymbolVal x & \case
+  SomeSymbol sym -> undefined {- SomeTypeLexicon {
+      parseType = combineTypeParsers
+          (\i -> if i == x
+              then Just $ SEOne sym
+              else Nothing)
+          _
+  } -}
+
+-- I need something of the form:
+combineTypeParsers :: (String -> Maybe (ShowableEnum '[x])) 
+  -> (String -> Maybe (ShowableEnum xs)) 
+  -> (String -> Maybe (ShowableEnum (x ': xs)))
+combineTypeParsers = undefined
 
 parseSomeLexicon :: [(String, String)] -> SomeTypeLexicon -> Either ParseError SomeLexicon
 parseSomeLexicon [(x,y)] = undefined
 parseSomeLexicon _ = undefined
 
 data ShowableType (s :: Symbol) = ShowableType (Proxy s)
-
-instance PartialOrd (ShowableType s) where
-    (<=) x y = True
 
 instance KnownSymbol s => Show (ShowableType s) where
     show (ShowableType p) = symbolVal p 
@@ -83,67 +82,79 @@ data Sum x y =
       Sum1 x
     | Sum2 y
 
-data ShowableEnum (ss :: [Symbol]) = 
-   ShowableEnum (FMapShowableType ss)
+instance (Show x, Show y) => Show (Sum x y) where
+    show (Sum1 x) = show x
+    show (Sum2 y) = show y
+
+data ShowableEnum (ss :: [Symbol]) where
+    SEOne  :: Proxy s -> ShowableEnum '[s]
+    SECons :: Proxy s -> ShowableEnum ss -> ShowableEnum (s ': ss)
+
+instance KnownSymbol s => Show (ShowableEnum '[s]) where
+    show (SEOne p) = symbolVal p
+
+instance (KnownSymbol s, Show (ShowableEnum (s2 ': ss))) => Show (ShowableEnum (s ': s2 ': ss)) where
+    show (SECons p xs) = symbolVal p
+
+instance PartialOrd (ShowableEnum '[s]) where
+    (<=) x y = True
 
 data SomeShowableEnum = forall (ss :: [Symbol]). 
     SomeShowableEnum (ShowableEnum ss)
 
 ------------- Parsers --------------
 
-{-
 comment = do
     char '%'
     many (char ' ')
     char '\n'
 
+token :: Parsec String () t -> Parsec String () ()
 token p = do
     many (char ' ' <|> char '\n' <|> comment)
-    x <- p
+    _ <- p
     many (char ' ' <|> char '\n' <|> comment)
-    return x
+    return ()
 
-or       = token (char '|')
-equals   = token (char '=')
-end      = token (char '.')
-sutypeOf = token (string ":<") 
-typeOf = token (string ":") 
-comma    = token (char ',')
-arrow    = token (string "-->")
+orT       = token (char '|')
+equals    = token (char '=')
+end       = token (char '.')
+subtypeOf = token (string ":<") 
+typeOfT   = token (char ':') 
+comma     = token (char ',')
+arrow     = token (string "-->")
 typeToken = token (string "Type")
 
-entity = do
-    lowercase
+entityT = do
+    lower
     many alphaNum
 
-text = many alphaNum
+textT = many alphaNum
 
 typeDeclaration = do
     typeToken
     equals
-    sepBy1 $ do
-        x <- entity
-        or
-        return x
+    sepBy1 orT entityT
 
 subtypeDeclaration = do
-    x <- entity
+    x <- entityT
     subtypeOf
-    y <- entity
+    y <- entityT
     return (x, y)
 
 atomDeclaration = do
-    x <- entity
-    typeOf
-    y <- entity
+    x <- entityT
+    typeOfT
+    y <- entityT
     return (x, y)
 
 productionDeclaration = do
-    x <- text 
+    x <- textT
     arrow
-    y <- entity
+    y <- entityT
     return (x, y)
 
+{-
 -- TODO: Need to do some validation between these steps here.
 montagueSchema :: Parser SomeMontagueSchema
 montagueSchema = do
