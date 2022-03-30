b541a1111f36fea1d8af7b5518ecc94d2c9925ce
Nathan Bedell <nbedell@tulane.edu>
2022-03-30 09:13:36 -0400

    Add frontend-lib to default.nix

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 , obelisk ? import ./.obelisk/impl {
     inherit system;
     iosSdkVersion = "13.2";
-    
+
 	  config.android_sdk.accept_license = true;
 
     # In order to use Let's Encrypt for HTTPS deployments you must accept
@@ -20,12 +20,16 @@ project ./. ({ pkgs, ... }: {
   ios.bundleIdentifier = "org.bedelibry.demos.montague";
   ios.bundleName = "Montague App";
 
+  packages = {
+    frontend-lib = ./frontend-lib;
+  }
+
   overrides = self: super: {
       monad-tree = self.callHackageDirect {
         pkg = "monad-tree";
         ver = "0.2.0.0";
         sha256 = "qU50YWyeM1QI3lGQwboJ0iUlC4c4YTOrv3u/aVagRlg=";
-      } {}; 
+      } {};
       montague = self.callCabal2nix "montague" (pkgs.fetchFromGitHub {
         owner = "sintrastes";
         repo = "montague";
