dcc8cc0240f4586092299e39fba4f63238fded4f
Nathan Bedell <nbedell@tulane.edu>
2022-03-31 08:44:20 -0400

    Try instance with performable.

--- frontend/src-bin/main.hs
+++ frontend/src-bin/main.hs
@@ -11,7 +11,7 @@ import Obelisk.Route.Frontend
 import Obelisk.Generated.Static
 import Reflex.Dom
 
-instance ObeliskWidget t route m => MonadToast m where
+instance ObeliskWidget t route m => MonadToast (Performable m) where
     toast message = do
         prerender_ blank $ 
             liftJSM $ eval ("M.toast({html: '" <> message <> "'})" :: T.Text)
