3dbf497500c4b4bb366909b660688673b277ae86
Nathan Bedell <nbedell@tulane.edu>
2022-04-04 21:03:57 -0400

    Fix wrapComponents.

--- frontend-lib/src/Montague/Frontend/TabDisplay.hs
+++ frontend-lib/src/Montague/Frontend/TabDisplay.hs
@@ -8,17 +8,18 @@ data TabF m a where
     Tab :: T.Text -> m r -> (r -> m a) -> TabF m a
 
 instance Functor (TabF m) where
-    fmap f (Tab label widget rest) = Tab label widget (\x -> f <$> rest x)
+    fmap f (Tab label widget rest) = Tab label widget (fmap f . rest)
 
 type Tab m = Free (TabF m)
 
 -- Get the labels associated with the tabs.
 labels :: Tab m a -> [T.Text]
 labels (Pure _) = []
-labels (Free (Tab label res rest)) = label:labels (rest res)
+labels (Free (Tab label res rest)) = label:labels (fmap res . rest)
 
 wrapComponents :: Monad m => (forall r. T.Text -> m r -> m r) -> Tab m () -> m ()
 wrapComponents wrap (Pure _) = pure ()
 wrapComponents wrap (Free (Tab label x rest)) = do
     res <- wrap label x
-    wrapComponents wrap (rest res)
+    rest' <- rest res
+    wrapComponents wrap rest'
