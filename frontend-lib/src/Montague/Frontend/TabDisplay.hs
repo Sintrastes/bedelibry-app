e44b10946e4acc74aa8e2ef3e3dc08a111a23665
Nathan Bedell <nbedell@tulane.edu>
2022-04-04 21:00:16 -0400

    Add implementation for wrapComponents.

--- frontend-lib/src/Montague/Frontend/TabDisplay.hs
+++ frontend-lib/src/Montague/Frontend/TabDisplay.hs
@@ -1,14 +1,14 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs, RankNTypes #-}
 module Montague.Frontend.TabDisplay where
 
 import qualified Data.Text as T
 import Control.Monad.Free
 
 data TabF m a where
-    Tab :: T.Text -> m r -> (r -> a) -> TabF m a
+    Tab :: T.Text -> m r -> (r -> m a) -> TabF m a
 
 instance Functor (TabF m) where
-    fmap f (Tab label widget rest) = Tab label widget (f . rest)
+    fmap f (Tab label widget rest) = Tab label widget (\x -> f <$> rest x)
 
 type Tab m = Free (TabF m)
 
@@ -16,3 +16,9 @@ type Tab m = Free (TabF m)
 labels :: Tab m a -> [T.Text]
 labels (Pure _) = []
 labels (Free (Tab label _ rest)) = label:labels rest
+
+wrapComponents :: Monad m => (forall r. T.Text -> m r -> m r) -> Tab m () -> m ()
+wrapComponents wrap (Pure _) = pure ()
+wrapComponents wrap (Free (Tab label x rest)) = do
+    res <- wrap label x
+    wrapComponents wrap (rest res)
