865d1e5b6a075c63359f13cc58ae25e0c861acba
Nathan Bedell <nbedell@tulane.edu>
2022-04-06 09:14:37 -0400

    Add a field to preferences for dark mode.

--- frontend-lib/src/Montague/Frontend/Preferences.hs
+++ frontend-lib/src/Montague/Frontend/Preferences.hs
@@ -11,17 +11,22 @@ import Reflex.Dom.Core
 import Data.Functor
 
 data Preferences = Preferences {
-    style :: Style
+    style :: Style,
+    darkMode :: Bool
 }
 
 preferencePage :: _ => m (Dynamic t Preferences)
 preferencePage = do
-    p $ text "Hello"
     checkboxValue <- el "form" $ el "p" $ el "label" $ do
         el "span" $ text "Filled in"
         _checkbox_value <$> checkbox True def
-    pure $ checkboxValue <&> 
-      (\case
-        True  -> Android
-        False -> IOS) <&>
-      Preferences
+    
+    let style = checkboxValue <&> 
+          (\case
+            True  -> Android
+            False -> IOS)
+
+    return $ 
+        Preferences <$> 
+          style <*>
+          pure False
