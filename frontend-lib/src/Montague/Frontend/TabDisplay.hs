d50b6285f03fd9a728f17d7d37620c48f09807bb
Nathan Bedell <nbedell@tulane.edu>
2022-04-07 20:54:47 -0400

    Make tab display generic in the type of label.

--- frontend-lib/src/Montague/Frontend/TabDisplay.hs
+++ frontend-lib/src/Montague/Frontend/TabDisplay.hs
@@ -7,22 +7,22 @@ import Reflex.Dom.Core hiding(Tab)
 import Control.Monad.Fix
 import Data.Functor
 
-data TabF m a where
-    Tab :: T.Text -> m r -> (r -> a) -> TabF m a
+data TabF e m a where
+    Tab :: e -> m r -> (r -> a) -> TabF e m a
 
-instance Functor m => Functor (TabF m) where
+instance Functor m => Functor (TabF e m) where
     fmap f (Tab label widget rest) = Tab label widget (f . rest)
 
-type Tab m = Free (TabF m)
+type Tab e m = Free (TabF e m)
 
-tab :: Monad m => T.Text -> m a -> Tab m a
+tab :: Monad m => e -> m a -> Tab e m a
 tab label x = liftF (Tab label x id)
 
-tabDisplay :: (MonadFix m, MonadHold t m, DomBuilder t m, PostBuild t m) =>
-     T.Text
-  -> [T.Text]
-  -> Event t T.Text
-  -> Tab m a
+tabDisplay :: (Eq e, MonadFix m, MonadHold t m, DomBuilder t m, PostBuild t m) =>
+     e
+  -> [e]
+  -> Event t e
+  -> Tab e m a
   -> m a
 tabDisplay defaultTab tabs navEvents = tabDisplay' defaultTab tabs navEvents wrap
   where
@@ -34,20 +34,20 @@ tabDisplay defaultTab tabs navEvents = tabDisplay' defaultTab tabs navEvents wra
         elDynAttr "div" (displayAttrs navEvents label) x
 
 tabDisplay' :: (MonadHold t m, MonadFix m) =>
-     T.Text
-  -> [T.Text]
-  -> Event t T.Text
-  -> (forall r. Dynamic t T.Text -> T.Text -> m r -> m r)
-  -> Tab m a
+     e
+  -> [e]
+  -> Event t e
+  -> (forall r. Dynamic t e -> e -> m r -> m r)
+  -> Tab e m a
   -> m a
 tabDisplay' defaultTab tabs navEvents wrap tab = mdo
     currentTab <- holdDyn defaultTab navEvents
     wrapComponents wrap currentTab tab
 
 wrapComponents :: Monad m =>
-    (forall r. Dynamic t T.Text -> T.Text -> m r -> m r)
- -> Dynamic t T.Text
- -> Tab m a
+    (forall r. Dynamic t e -> e -> m r -> m r)
+ -> Dynamic t e
+ -> Tab e m a
  -> m a
 wrapComponents wrap navEvents (Pure x) = pure x
 wrapComponents wrap navEvents (Free (Tab label x rest)) = do
