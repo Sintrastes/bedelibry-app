8751e9f0de2cc17c103867ec26b2cb178dcb584c
Nathan Bedell <nbedell@tulane.edu>
2022-04-14 09:02:03 -0400

    Try commenting out javascript.

--- m1-frontend/src/main.hs
+++ m1-frontend/src/main.hs
@@ -11,10 +11,10 @@ header = do
     "href" =: "https://sintrastes.github.io/demos/montague/w3.css" <>
     "type" =: "text/css" <>
     "rel" =: "stylesheet") blank
-  elAttr "script" (
-    "src" =: "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js") blank
-  elAttr "script" (
-    "src" =: "https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js") blank
+  -- elAttr "script" (
+  --   "src" =: "https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js") blank
+  -- elAttr "script" (
+  --   "src" =: "https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js") blank
   elAttr "link" (
     "id" =: "css-style" <>
     "href" =: "https://sintrastes.github.io/demos/montague/materialize.min.css" <>
