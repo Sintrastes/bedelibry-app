be3cd39d28f91226b471dfa92c6caab2f54c048b
Nathan Bedell <nbedell@tulane.edu>
2022-04-15 20:40:57 -0400

    Fix typo in console.log.

--- frontend-lib/src/Montague/Frontend/Utils/Javascript.hs
+++ frontend-lib/src/Montague/Frontend/Utils/Javascript.hs
@@ -19,7 +19,7 @@ removeWithId id = do
 modifyLink :: _ => T.Text -> T.Text -> m ()
 modifyLink id url = do
     liftJSM $ eval (
-        (" consolve.log(\"modifyLink\"); var element = document.getElementById('" <> id <> "'); \
+        (" console.log(\"modifyLink\"); var element = document.getElementById('" <> id <> "'); \
         \ element.setAttribute('href','" <> url <> "'); \
         \") :: T.Text)
     pure ()
