aaf9a57ce1dd6e11fa0364290bfad01d83debdd8
Nathan Bedell <nbedell@tulane.edu>
2022-04-20 09:01:27 -0400

    Add empty view for entity page.

--- frontend-lib/src/Montague/Frontend/Entity.hs
+++ frontend-lib/src/Montague/Frontend/Entity.hs
@@ -33,23 +33,39 @@ import Control.Monad
 
 entityPage ::  _ => Dynamic t Style -> Dynamic t (Maybe SomeLexicon) -> m ()
 entityPage style maybeParsedSchema = let ?style = style in do
-    elClass "ul" "collection" $
-        dyn $ maybeParsedSchema <&> \case
-            Nothing -> pure ()
-            Just (SomeLexicon pT pA semantics) -> do
-                let entities = getEntities pA
-                let types = fmap (bfs . typeOfAtom semantics) entities
-                let typingPairs = zip entities types
-                forM_ typingPairs (\(entity, typ) -> do
-                    elClass "li" "collection-item" $ do
-                        el "span" $ text $
-                            T.pack $ show entity <> ": "
-                        elAttr "span" ("style" =: "float: right;") $ text $
-                            T.pack $ intercalate " | " . fmap show $ typ)
+    dyn $ maybeParsedSchema <&> \case
+        Nothing -> elAttr "div" centerContent $
+            el "div" $ do
+                elClass "p" grayText $ text "No schema has been specified."
+                elClass "p" grayText $ text "You need to enter one in the schema page."
+        Just (SomeLexicon pT pA semantics) -> do
+            let entities = getEntities pA
+            let types = fmap (bfs . typeOfAtom semantics) entities
+            let typingPairs = zip entities types
+            case entities of
+                [] -> elAttr "div" centerContent $
+                    el "div" $ do
+                        elClass "p" grayText $ text "No entities have been specified."
+                _ -> elClass "ul" "collection" $ 
+                    forM_ typingPairs (\(entity, typ) -> do
+                        elClass "li" "collection-item" $ do
+                            el "span" $ text $
+                                T.pack $ show entity <> ": "
+                            elAttr "span" ("style" =: "float: right;") $ text $
+                                T.pack $ intercalate " | " . fmap show $ typ)
     pure ()
   where
     getEntities :: (Bounded a, Enum a) => Proxy a -> [a]
     getEntities Proxy = enumValues
+
+    grayText = "unselectable grey-text text-lighten-1"
+
+    centerContent =
+        "style" =: ("text-align: center;" <>
+            "min-height:100%;" <> 
+            "display:flex;" <> 
+            "justify-content:center;" <> 
+            "align-items:center;")
 
 enumValues :: (Bounded a, Enum a) => [a]
 enumValues = [minBound..maxBound]
