6d218bfdc35d824f7589ce108662579a9db86f07
Nathan Bedell <nbedell@tulane.edu>
2022-05-07 13:07:02 -0400

    Add dependency on meriv-core.

--- default.nix
+++ default.nix
@@ -37,5 +37,11 @@ project ./. ({ pkgs, ... }: {
         rev = "55f22d3efa2fc1565bac70f6cc1bb8cd9397f721";
         sha256 = "J5smYPhshQZwD6Qn4YIvjBQPXn1/5UDAkgyP+8QT6aI=";
       }) {};
+      meriv-core = self.callCabal2nix "montague" (pkgs.fetchFromGitHub {
+        owner = "sintrastes";
+        repo = "meriv-core";
+        rev = "413ac017684fcf59474a6cc2dccb9fb6d33825f1";
+        sha256 = "J5smYPhshQZwD6Qn4YIvjBQPXn1/5UDAkgyP+8QT6aI=";
+      }) {};
     };
 })
