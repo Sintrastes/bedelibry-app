8ec7c7e0be5eb6d38424afd27aabbabf2827fb95
Nathan Bedell <nbedell@tulane.edu>
2022-05-10 20:20:04 -0400

    Add definition of MdDoc.

--- frontend-lib/src/Montague/Parser/Markdown.hs
+++ frontend-lib/src/Montague/Parser/Markdown.hs
@@ -1,6 +1,34 @@
 
 module Montague.Parser.Markdown where
 
+import Text.Parsec
+import qualified Data.Text as T
+
 -- | Specailized markdown parser allowing for annotations
 -- in a logic programming language.
 
+data MdDoc a = MarkdownDoc [MdSection a]
+
+data MdSection a =
+    BigHeader T.Text
+  | SmallHeader T.Text
+  | Paragraph (MdParagraph a)
+
+data MdParagraph a = MarkdownParagraph [MdParagraphComponent a]
+
+data MdParagraphComponent a = 
+    RawText T.Text
+  | Italic (MdParagraphComponent a)
+  | Bold (MdParagraphComponent a)
+  | Annotated a T.Text
+
+token :: Parsec String () t -> Parsec String () t
+token p = do
+    many (try (char ' ' >> pure ()) <|> (char '\n' >> pure ()))
+    res <- p
+    many (try (char ' ' >> pure ()) <|> (char '\n' >> pure ()))
+    return res
+
+header = undefined
+
+document = undefined
