3b416c65124d0fa8a1c5afbde5642753ff57ead6
Nathan Bedell <nbedell@tulane.edu>
2022-05-11 08:41:29 -0400

    Initial implementation of markdown parser.

--- frontend-lib/src/Montague/Parser/Markdown.hs
+++ frontend-lib/src/Montague/Parser/Markdown.hs
@@ -1,20 +1,22 @@
 
 module Montague.Parser.Markdown where
 
-import Text.Parsec
+import Text.Parsec hiding (token)
 import qualified Data.Text as T
 
 -- | Specailized markdown parser allowing for annotations
 -- in a logic programming language.
 
-data MdDoc a = MarkdownDoc [MdSection a]
+newtype MdDoc a = 
+    MdDoc [MdSection a]
 
 data MdSection a =
     BigHeader T.Text
   | SmallHeader T.Text
   | Paragraph (MdParagraph a)
 
-data MdParagraph a = MarkdownParagraph [MdParagraphComponent a]
+newtype MdParagraph a = 
+    MdParagraph [MdParagraphComponent a]
 
 data MdParagraphComponent a = 
     RawText T.Text
@@ -29,6 +31,22 @@ token p = do
     many (try (char ' ' >> pure ()) <|> (char '\n' >> pure ()))
     return res
 
-header = undefined
+document :: Parsec String () (MdDoc ())
+document = MdDoc <$> many section
 
-document = undefined
+section :: Parsec String () (MdSection ())
+section = try smallHeader <|>
+    Paragraph <$> paragraph
+
+smallHeader :: Parsec String () (MdSection ())
+smallHeader = SmallHeader <$> do
+    token (char '#')
+    T.pack <$> manyTill anyChar (try $ char '\n')
+
+paragraph :: Parsec String () (MdParagraph ())
+paragraph = MdParagraph <$> 
+    many paragraphComponent
+
+paragraphComponent :: Parsec String () (MdParagraphComponent ())
+paragraphComponent = RawText . T.pack <$>
+    manyTill anyChar (try $ char '\n')
