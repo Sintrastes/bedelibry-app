3d8e27769564851c9781ce715b10d3ded4edfcb1
Nathan Bedell <nbedell@tulane.edu>
2022-05-12 09:12:40 -0400

    More tweaks to markdown parser.

--- frontend-lib/src/Bedelibry/Parser/Markdown.hs
+++ frontend-lib/src/Bedelibry/Parser/Markdown.hs
@@ -3,50 +3,66 @@ module Bedelibry.Parser.Markdown where
 
 import Text.Parsec hiding (token)
 import qualified Data.Text as T
+import Data.Functor
 
 -- | Specailized markdown parser allowing for annotations
 -- in a logic programming language.
 
-newtype MdDoc a = 
+newtype MdDoc a =
     MdDoc [MdSection a]
+        deriving(Show)
 
 data MdSection a =
     BigHeader T.Text
   | SmallHeader T.Text
   | Paragraph (MdParagraph a)
+      deriving(Show)
 
-newtype MdParagraph a = 
-    MdParagraph [MdParagraphComponent a]
+type MdParagraph a = [MdParagraphComponent a]
 
-data MdParagraphComponent a = 
+data MdParagraphComponent a =
     RawText T.Text
   | Italic (MdParagraphComponent a)
   | Bold (MdParagraphComponent a)
   | Annotated a T.Text
+      deriving(Show)
 
 token :: Parsec String () t -> Parsec String () t
 token p = do
-    many (try (char ' ' >> pure ()) <|> (char '\n' >> pure ()))
+    whitespace
     res <- p
-    many (try (char ' ' >> pure ()) <|> (char '\n' >> pure ()))
+    whitespace
     return res
 
+whitespace = do
+    many $
+        try (char ' ' >> pure ()) <|>
+            (char '\n' >> pure ())
+    pure ()
+
 document :: Parsec String () (MdDoc ())
-document = MdDoc <$> many section
+document = MdDoc <$>
+    section `sepBy` (char '\n' >> whitespace)
 
 section :: Parsec String () (MdSection ())
 section = try smallHeader <|>
+    try bigHeader <|>
     Paragraph <$> paragraph
 
 smallHeader :: Parsec String () (MdSection ())
 smallHeader = SmallHeader <$> do
-    token (char '#')
-    T.pack <$> manyTill anyChar (try $ char '\n')
+    token (string "##")
+    T.pack <$> many1 (try $ noneOf "\n")
+
+bigHeader :: Parsec String () (MdSection ())
+bigHeader = BigHeader <$> do
+    token (string "#")
+    T.pack <$> many1 (try $ noneOf "\n")
 
 paragraph :: Parsec String () (MdParagraph ())
-paragraph = MdParagraph <$> 
-    many paragraphComponent
+paragraph = (: []) <$>
+    paragraphComponent
 
 paragraphComponent :: Parsec String () (MdParagraphComponent ())
 paragraphComponent = RawText . T.pack <$>
-    manyTill anyChar (try $ char '\n')
+    many1 (try $ noneOf "\n")
